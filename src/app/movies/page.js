ce8c0b03d71ecbb41ddd80724e2b37072b537732
Mohammed <sajidcurious9@gmail.com>
2023-07-12 02:17:13 +0530

    added rapid api to it

--- src/app/movies/page.js
+++ src/app/movies/page.js
@@ -1,6 +1,14 @@
 import React from "react";
 
 const page = () => {
+  const url = process.env.API_KEY;
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "9f60731615mshb634896df8a3394p19d915jsn14105988137b",
+      "X-RapidAPI-Host": "netflix54.p.rapidapi.com",
+    },
+  };
   return <div>movies</div>;
 };
 
