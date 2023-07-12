62366be83c9ac5cd926f1f8f27a53ec54d6ca39e
Mohammed <sajidcurious9@gmail.com>
2023-07-13 03:06:37 +0530

    styling the card

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -18,32 +18,28 @@ const MovieCard = (curElem) => {
                   height={400}
                 ></Image>
                 <div class="p-6">
-                  <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
-                    CATEGORY
-                  </h2>
                   <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
-                    The Catalyzer
+                    {title}
                   </h1>
-                  <p class="leading-relaxed mb-3">
-                    Photo booth fam kinfolk cold-pressed sriracha leggings
-                    jianbing microdosing tousled waistcoat.
-                  </p>
+                  <p class="leading-relaxed mb-3">{synopsis}</p>
                   <div class="flex items-center flex-wrap ">
-                    <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
-                      Learn More
-                      <svg
-                        class="w-4 h-4 ml-2"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        fill="none"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      >
-                        <path d="M5 12h14"></path>
-                        <path d="M12 5l7 7-7 7"></path>
-                      </svg>
-                    </a>
+                    <Link href={`/movies/${id}`}>
+                      <button class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
+                        Learn More
+                        <svg
+                          class="w-4 h-4 ml-2"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          stroke-width="2"
+                          fill="none"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        >
+                          <path d="M5 12h14"></path>
+                          <path d="M12 5l7 7-7 7"></path>
+                        </svg>
+                      </button>
+                    </Link>
                   </div>
                 </div>
               </div>
