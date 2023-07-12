fdfe6a765c3eef9b92004025a2ef5830e5d46ff8
Mohammed <sajidcurious9@gmail.com>
2023-07-13 02:51:54 +0530

    added and made the card for movie and solved image importing proble for online images with ling

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,7 +1,58 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-const MovieCard = () => {
-  return <div>MovieCard</div>;
+const MovieCard = (curElem) => {
+  const { id, type, title, synopsis } = curElem.jawSummary;
+  return (
+    <>
+      <section class="text-gray-600 body-font">
+        <div class="container px-5 py-24 mx-auto">
+          <div class="flex flex-wrap -m-4">
+            <div class="p-4 md:w-1/3">
+              <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                <Image
+                  src={curElem.jawSummary.backgroundImage.url}
+                  alt="title"
+                  width={720}
+                  height={400}
+                ></Image>
+                <div class="p-6">
+                  <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                    CATEGORY
+                  </h2>
+                  <h1 class="title-font text-lg font-medium text-gray-900 mb-3">
+                    The Catalyzer
+                  </h1>
+                  <p class="leading-relaxed mb-3">
+                    Photo booth fam kinfolk cold-pressed sriracha leggings
+                    jianbing microdosing tousled waistcoat.
+                  </p>
+                  <div class="flex items-center flex-wrap ">
+                    <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
+                      Learn More
+                      <svg
+                        class="w-4 h-4 ml-2"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        fill="none"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      >
+                        <path d="M5 12h14"></path>
+                        <path d="M12 5l7 7-7 7"></path>
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default MovieCard;
