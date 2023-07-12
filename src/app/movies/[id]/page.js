0c9bf5e6734d8ec56a139f67ddf21736309dad3c
Mohammed <sajidcurious9@gmail.com>
2023-07-13 03:05:35 +0530

    adding dynamic routes to the movie page

--- src/app/movies/[id]/page.js
+++ src/app/movies/[id]/page.js
@@ -1,7 +1,8 @@
 import React from "react";
 
-const page = () => {
-  return <div>I am dynamic route</div>;
+const page = ({ params }) => {
+  const id = params.id;
+  return <div>I am dynamic route{id}</div>;
 };
 
 export default page;
