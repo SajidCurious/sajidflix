ca1219a0c0d9da10e3cb711c2ab53ae15cb3c29e
Mohammed <sajidcurious9@gmail.com>
2023-07-23 03:52:34 +0530

    made height of hero component to 100vh

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ import React from "react";
 const Hero = ({ title, imageUrl }) => {
   return (
     <section class="text-gray-600 body-font bg-blue-100">
-      <div class="container mx-auto flex px-10 lg:p-40 py-24 md:flex-row flex-col items-center">
+      <div class="container mx-auto flex px-10 lg:p-40 py-24 md:flex-row flex-col items-center h-[100vh]">
         <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 class="title-font sm:text-4xl text-3xl mb-4 font-bold text-gray-900">
             {title}
