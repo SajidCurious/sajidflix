562502977223459d977011e1945666c08c6a73ab
Mohammed <sajidcurious9@gmail.com>
2023-07-23 03:51:41 +0530

    edited header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,31 +5,28 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header class="body-font bg-[#222f3e] text-white w-full">
+    <header class="body-font bg-[#222f3e] text-white w-full ">
       <div class="container mx-auto flex flex-wrap p-4 flex-col md:flex-row items-center w-full">
         <Link href="/">
-          <span class="ml-3 text-xl font-semibold text-white cursor-pointer">
+          <span class="ml-3 text-xl font-semibold text-white cursor-pointer sm:mb-32">
             SAJIDFLIX
           </span>
         </Link>
-        <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center list-none font-semibold">
+        <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center list-none font-semibold space-x-10">
           <li>
-            <Link href="/" className="mr-5 hover:text-blue-200 cursor-pointer">
+            <Link href="/" className=" hover:text-blue-200 cursor-pointer">
               Home
             </Link>
           </li>
           <li>
-            <Link
-              href="/about"
-              className="mr-5 hover:text-blue-200 cursor-pointer"
-            >
+            <Link href="/about" className=" hover:text-blue-200 cursor-pointer">
               About
             </Link>
           </li>
           <li>
             <Link
               href="/movies"
-              className="mr-5 hover:text-blue-200 cursor-pointer"
+              className=" hover:text-blue-200 cursor-pointer"
             >
               Movies
             </Link>
