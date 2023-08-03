d21bd338850d153ceee83e984e1dc41bc7b439fa
Mohammed <sajidcurious9@gmail.com>
2023-08-04 00:57:37 +0530

    removing unwanted code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,57 +2,59 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer class="text-white body-font bg-[#222f3e]">
-      <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-        <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-          <span class="ml-3 text-xl text-white font-semibold">SajidFlix</span>
+    <footer className="text-white body-font bg-[#222f3e]">
+      <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+        <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+          <span className="ml-3 text-xl text-white font-semibold">
+            SajidFlix
+          </span>
         </a>
-        <p class="text-sm  sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
+        <p className="text-sm  sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
           © 2023 SajidFlix —
           <a
             href="https://twitter.com/@sajid_curious"
-            class="ml-1"
+            className="ml-1"
             rel="noopener noreferrer"
             target="_blank"
           >
             @sajid_curious
           </a>
         </p>
-        <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+        <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
           <a href="https://twitter.com/@sajid_curious">
             <svg
               fill="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              class="w-5 h-5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-5 h-5"
               viewBox="0 0 24 24"
             >
               <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
             </svg>
           </a>
-          <a class="ml-6 ">
+          <a className="ml-6 ">
             <svg
               fill="none"
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              class="w-5 h-5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-5 h-5"
               viewBox="0 0 24 24"
             >
               <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
               <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
             </svg>
           </a>
-          <a href="https://www.linkedin.com/in/sajidcurious/" class="ml-6 ">
+          <a href="https://www.linkedin.com/in/sajidcurious/" className="ml-6 ">
             <svg
               fill="currentColor"
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="0"
-              class="w-5 h-5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="0"
+              className="w-5 h-5"
               viewBox="0 0 24 24"
             >
               <path
